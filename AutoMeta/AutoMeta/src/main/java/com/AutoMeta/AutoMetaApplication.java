41c0608ae28dfdc81fdbd1e90c310e8768a7bfcc
Neelmani Bhardwaj <neelmanibhardwaj003@gmail.com>
2025-10-05 22:27:30 -0400

    making html files

---
+++ AutoMeta/AutoMeta/src/main/java/com/AutoMeta/AutoMetaApplication.java
@@ -0,0 +1,13 @@
+package com.AutoMeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoMetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoMetaApplication.class, args);
+	}
+
+}
